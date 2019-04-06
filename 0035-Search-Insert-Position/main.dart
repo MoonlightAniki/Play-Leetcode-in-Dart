15091535e678965f5df1728a414eb7f97af0c724
guoliang <guoliang@kuaishou.com>
2019-04-06 22:24:54 +0800

    35. Search Insert Position

---
+++ 0035-Search-Insert-Position/main.dart
@@ -0,0 +1,52 @@
+// 35. Search Insert Position
+// https://leetcode.com/problems/search-insert-position/
+/*
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Example 1:
+Input: [1,3,5,6], 5
+Output: 2
+
+Example 2:
+Input: [1,3,5,6], 2
+Output: 1
+
+Example 3:
+Input: [1,3,5,6], 7
+Output: 4
+
+Example 4:
+Input: [1,3,5,6], 0
+Output: 0
+ */
+
+int searchInsert(List<int> nums, int target) {
+  if (nums == null) {
+    return -1;
+  }
+  if (nums.isEmpty) {
+    return 0;
+  }
+  int left = 0;
+  int right = nums.length - 1;
+  while (left <= right) {
+    int mid = (left + right) ~/ 2;
+    if (target == nums[mid]) {
+      return mid;
+    } else if (target < nums[mid]) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
+}
+
+void main() {
+  print(searchInsert([1, 3, 5, 6], 5));
+  print(searchInsert([1, 3, 5, 6], 2));
+  print(searchInsert([1, 3, 5, 6], 7));
+  print(searchInsert([1, 3, 5, 6], 0));
+}
